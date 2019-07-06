b5bbca697dd7d6319c1070f7b6ad7e5d2094d497
SEC\user <dorothy_007@naver.com>
2019-07-06 20:06:56 +0900

    컴포넌트에 연결
    connect, bindActionCreators 사용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as counterActions from './modules/counter';
 
 class App extends Component {
   render() {
+    const { CounterActions, number } = this.props;
     return(
       <div>
-        <h1>number</h1>
-        <button>+</button>
-        <button>-</button>
+        <h1>{number}</h1>
+        <button onClick={CounterActions.increment}>+</button>
+        <button onClick={CounterActions.decrement}>+</button>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  (state) => ({
+    number: state.counter
+  }),
+  (dispatch) => ({
+    CounterActions: bindActionCreators(counterActions, dispatch)
+  })
+)(App);
