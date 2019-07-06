d0ffda903a6a68ec37059106f253c9e2d5070f94
SEC\user <dorothy_007@naver.com>
2019-07-06 19:34:41 +0900

    기본 구조

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>number</h1>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    );
+  }
+}
+
+export default App;
